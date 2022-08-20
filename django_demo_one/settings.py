33d9bfdc70dc546d953ac04960ebbb3f1d2aead0
396220003@qq.com <396220003@qq.com>
2022-08-20 10:35:34 +0800

    update

--- django_demo_one/settings.py
+++ django_demo_one/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&gy8nfhnwt%(mr&gw347zacq%)xr5jo$+p-ic$*1ra!((*m9ce
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
